b7c21c494718b263f3cd8a02c63a48dbae62e36f
Matias Alochis <alochism@gmail.com>
2025-08-23 19:10:49 -0300

    "Actualizado Ejercicio5"

--- src/_02_Programacion_Estructurada/Ejercicio5.java
+++ src/_02_Programacion_Estructurada/Ejercicio5.java
@@ -1,13 +1,20 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 5. Suma de Números Pares (while).
+Escribe un programa que solicite números al usuario y sume solo los números pares. El ciclo debe continuar hasta
+que el usuario ingrese el número 0, momento en el que se debe mostrar la suma total de los pares ingresados.
+Ejemplo de entrada/salida:
+Ingrese un número (0 para terminar): 4
+Ingrese un número (0 para terminar): 7
+Ingrese un número (0 para terminar): 2
+Ingrese un número (0 para terminar): 0
+La suma de los números pares es: 6
  */
 package _02_Programacion_Estructurada;
 
-/**
- *
- * @author Matias
- */
 public class Ejercicio5 {
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
