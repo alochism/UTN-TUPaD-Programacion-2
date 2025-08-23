882384b059206691ae3fcdf58c24684e382403f5
Matias Alochis <alochism@gmail.com>
2025-08-23 16:55:12 -0300

    Terminado Ejercicio 1

---
+++ src/_01_Introduccion_a_Java/Ejercicio4.java
@@ -0,0 +1,36 @@
+/*
+4. Escribe un programa que solicite al usuario ingresar su nombre y edad, y luego 
+los muestre en pantalla. Usa Scanner para capturar los datos.
+*/
+
+
+package _01_Introduccion_a_Java;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+        public static void main(String [] args) {
+           //Defino el objeto tipo Scanner
+           Scanner input = new Scanner(System.in);
+           
+           //Solicito que se ingrese nombre
+            System.out.println("Ingrese su nombre: ");
+            String nombre;
+                        
+            nombre = input.nextLine();
+            
+            //Solicito que se ingresen edad
+            System.out.println("Ingrese su edad: ");
+            int edad;
+            edad = input.nextInt();
+           
+            //Imprimo por pantalla los datos ingresados
+            System.out.println("Su nombre es: " + nombre);
+            System.out.println("Su edad es: " + edad + " años");
+            
+
+
+            
+    }
+            
+    
+}
---
+++ src/_02_Programacion_Estructurada/Ejercicio1.java
@@ -0,0 +1,52 @@
+/*
+ 1. Verificación de Año Bisiesto.
+Escribe un programa en Java que solicite al usuario un año y determine si es bisiesto. 
+Un año es bisiesto si es divisible por 4, pero no por 100, salvo que sea divisible por 400.
+ */
+package _02_Programacion_Estructurada;
+
+//Importo la librería Scanner 
+import java.util.Scanner;
+
+/**
+ *
+ * @author Matias
+ */
+public class Ejercicio1 {
+    
+    public static void main(String [] args) {
+        
+        // Defino el objeto tipo Scanner
+        Scanner input = new Scanner (System.in);
+        
+        // Defino la variable anio de tipo integer
+        int anio;
+        
+        //Solicito por consola que se ingrese un año
+        System.out.println("Ingrese un año: ");
+        
+        // La variable año se debe leer de lo ingresado por consola
+        anio = input.nextInt();
+        
+        // Uso el método numBisiesto en el main
+        if (numBisiesto(anio)){ 
+            System.out.println("El número " + anio + " es bisiesto");
+            
+        } else {
+            System.out.println("El número " + anio + " NO es bisiesto");
+        }
+    }
+    
+    // Creo un método que calcule si un número ingresado es bisiesto
+    public static boolean numBisiesto(int anio){
+            
+        if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0)){
+            return true;
+        }
+        else {
+                return false;
+        }
+    }
+}
+    
+
